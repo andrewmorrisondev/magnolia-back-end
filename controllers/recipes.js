9a88d34896ec694d78c49fc7b3a5d5b6e3b4350e
trentonwahr <trentwahr@gmail.com>
2023-05-12 10:30:49 -0400

    stub up routes, controller, and server

---
+++ controllers/recipes.js
@@ -0,0 +1,6 @@
+import { Profile } from "../models/profile.js"
+import { FamilyRecipe } from "../models/recipe.js"
+import { FamilyTree } from "../models/tree.js"
+
+
+export { }
---
+++ controllers/trees.js
@@ -0,0 +1,6 @@
+import { Profile } from "../models/profile.js"
+import { FamilyTree } from "../models/tree.js"
+import { FamilyRecipe } from "../models/recipe.js"
+
+
+export { }
